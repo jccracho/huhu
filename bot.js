f187b2bbced0ca935903ec869de994090088b307
jccracho <87005884+jccracho@users.noreply.github.com>
2022-06-04 14:31:19 +0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,6 @@
+new TelegramBot(token, {polling: true});    bit.js                                        new TelegramBot(token, {polling: true});
+const HTMLParser = require('node-html-parser');
+const TelegramBot = require('node-telegram-bot-api');
+const token = '5501659865:AAF_mxLAjrTpvmcsu3fGx1FSuDNHdwxLpA0';
+new TelegramBot(token, {polling: true});ing: true});
+
